42a4ac8b9352c3a0c6df0390c73371911dc6aae6
Arinda Joel <arindajoel24@icloud.com>
2026-06-11 02:43:53 +0300

    add solution

---
+++ bad.py
@@ -0,0 +1,13 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        low, high = 0, n
+        while low <= high:
+            mid = (low + high)// 2
+            if isBadVersion(mid) == True:
+                high = mid - 1
+            elif isBadVersion(mid) == False:
+                low = mid + 1
+        return low   
