c44afdd65e81fcdea79630213153692187228a09
Arinda Joel <arindajoel24@icloud.com>
2026-06-17 23:44:52 +0300

    add solution

---
+++ stack.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        character = {")": "(", "}": "{", "]": "["}
+        for c in s:
+            if c in character:
+                if stack and stack[-1] == character[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False      
