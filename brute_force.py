941ff38441a5e2c333de519641855a6ae16eb61d
Arinda Joel <arindajoel24@icloud.com>
2026-06-20 03:35:56 +0300

    add solution

---
+++ brute_force.py
@@ -0,0 +1,12 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = []
+        for i, n in enumerate(temperatures):
+            for j in range(i + 1, len(temperatures)):
+                if n < temperatures[j]:
+                    answer.append(j - i)
+                    break
+            else:
+                answer.append(0)
+        return answer
+            
